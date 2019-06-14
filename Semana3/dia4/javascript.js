50dc65d65afb14969fad58dc497942af062fd3e2
marcoxjocxs <marvillatorres02@gmail.com>
2019-06-13 21:25:35 -0500

    javascript

---
+++ Semana3/dia4/javascript.js
@@ -0,0 +1,75 @@
+console.log("Pruebita");
+/* con var se define una variable */
+var nombre;
+/* variable = valor */
+/* siempre pongan un ; al terminar las sentencias */
+nombre="Marco";
+
+var PrimerNombre="Marco", SegundoNombre="Jose"; ApellidoMaterno="Torres";
+console.log(PrimerNombre);
+var Nombre="Lucia";
+
+// Numeros/Number Texto/String Booleanos
+
+//Number
+
+var x=9;
+var y=3.1415;
+var e=10e10;
+
+console.log(x);
+console.log(y);
+console.log(e);
+
+//Texto= string
+
+var string='Hola';
+var caracter = 'A';
+
+console.log(string);
+console.log(caracter);
+
+
+// Variables booleanas
+var lapizzavaconpina=true;
+var piscoeschileno=false;
+
+console.log(lapizzavaconpina);
+console.log(piscoeschileno);
+
+//Valores Raros
+
+//Valor Infinity
+console.log(5/0);
+
+//Not a Number
+console.log(5/"papata");
+
+var algo;
+
+
+/* Operadores */
+ var n1=9;
+ var n2=7;
+var suma=n1+n2;
+var resta=n1-n2;
+var multiplicacion=n1*n2;
+var division = n1/n2;
+var residuo=n1%n2;
+
+console.log("Suma:      " + suma);
+console.log("Resta:     " + resta);
+console.log("Multiplicacion:  " + multiplicacion);
+console.log("Division:  " + division);
+console.log("Residuo:   " + residuo);
+/* OPERADOR UNARIO */
+
+var mitexto="Pizza con champiñones";
+var pedazosdepizza=8;
+console.log(typeof(mitexto));
+console.log(typeof(pedazosdepizza));
+
+//Imprimir caracteres extraños
+
+var otrotexto="\"Hola chicos ya \nse viene la pizza\"";
+console.log(otrotexto);
