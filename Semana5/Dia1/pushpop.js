a04340d668ee1ae6ea01fd34258f771fd93a9f2c
marcoxjocxs <marvillatorres02@gmail.com>
2019-06-24 22:08:58 -0500

    Semana5

---
+++ Semana5/Dia1/pushpop.js
@@ -0,0 +1,9 @@
+var arreglo = [0,5,10,1000]; 
+console.log(arreglo);
+arreglo.push(9);
+console.log(arreglo);
+arreglo.push(90);
+console.log(arreglo);
+console.log("Vamos retirar valores del arreglo");
+arreglo.pop();
+console.log(arreglo);
