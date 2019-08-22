b923f369b26ecaca6403035e87e1ded84ac95eca
crisgon123 <crisgon123@gmail.com>
2019-08-21 20:27:48 -0500

    a

--- Semana13/Dia1/05-eventos.js
+++ Semana13/Dia1/05-eventos.js
@@ -34,19 +34,82 @@ window.onload = function () {
     });
 
     //cUANDO EL MOUSE ESTE ENCIMA DEL ELEMENTO
-    $("#imagen").mouseover(function () {
+    /* $("#imagen").mouseover(function () {
         $(this).addClass("rounded-circle");
     });
     //Cando el mouse salga del elemento
     $("#imagen").mouseout(function () {
         $(this).removeClass("rounded-circle");
-    });
+    }); */
 
     //funcion hover > conbina mouseover y mouseout
 
-    $("#imagen").hover(function(){ $(this).addClass("rounded-circle");},function(){ $(this).removeClass("rounded-circle");})
+    $("#imagen").hover(function () { $(this).addClass("rounded-circle"); }, function () { $(this).removeClass("rounded-circle"); })
+
+
+    $("#inputBuscar").focus(function () {
+        $(this).removeClass("form-control");
+        $(this).addClass("barra-activa");
+    });
+
+    $("#inputBuscar").blur(function () {
+        $(this).removeClass("barra-activa");
+        $(this).addClass("form-control");
+    });
+    $("html").click(function(){
+        $("#miMenu").remove();
+    });
+
     
-    
+    $("#btnRon").contextmenu(function (e) {
+        e.preventDefault();
+        //remove => quita un elemento del DOM
+        $("#miMenu").remove();
+        var X = e.clientX;
+        var Y = e.clientY;
+        //fORMA DE CREAR  fACIL
+        /* $(`<ul class="list-group">
+                        <li class="list-group-item">Active item</li>
+                        <li class="list-group-item">Item</li>
+                        <li class="list-group-item ">Disabled item</li>
+                      </ul>`); */
+
+        // creando el elemento ul que sera el menu
+        var menu = $(`<ul></ul>`);
+        //attr => asigna el valor de un atributo
+        menu.attr("id","miMenu");
+        menu.addClass("list-group");
+
+        // creando una opcion (li) para el elemento ul
+        var opcion1 = $(`<li></li>`);
+        opcion1.addClass("list-group-item");
+        opcion1.html("OCULTAR");
+        opcion1.click(function(){
+            $("#btnRon").hide();
+        });
+        //creando una opcion(li) para el elemento ul
+        var opcionImprimir = $(`<li><</li>`);
+        opcionImprimir.addClass("list-group-item");
+        opcionImprimir.html("IMPRIMIR");
+        opcionImprimir.click(function(){
+            $("#miMenu").remove();
+            window.print();
+        })
+        
+        // inyectando el elemento li al elemento ul
+        menu.append(opcion1);
+        menu.append(opcionImprimir);
+        menu.css("position", "absolute")
+            .css("top", `${Y}px`)
+            .css("left", `${X}px`);
+
+        $("body").append(menu);
+
+        console.log("Se hizo un click derecho");
+
+    })
+
+
     reset();
 }
 
@@ -56,3 +119,4 @@ window.onload = function () {
 /* https://www.jsdelivr.com/package/npm/sweetalert2?path=dist&tab=collection */
 /* https://cdnjs.com/libraries/font-awesome/4.7.0 */
 /* https://bootswatch.com/ */
+/* Instalar colorpic */
---
+++ Semana13/Dia2/05-ajax.js
@@ -0,0 +1,10 @@
+var barradeChat = document.getElementById("Chat");
+var escribechat=document.getElementById("usermsg").value;
+var btnsubmitmsg=document.getElementById("submitmsg");
+
+btnsubmitmsg.onclick(function(){
+    console.log(escribechat.text);
+})
+
+
+
