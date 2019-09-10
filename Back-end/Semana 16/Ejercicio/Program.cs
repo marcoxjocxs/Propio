76a133b66eebd0deb78f65f47b760c8281229a21
crisgon123 <crisgon123@gmail.com>
2019-09-09 22:09:58 -0500

    Ejercicio semana 16 dia1

---
+++ Back-end/Semana 16/Ejercicio/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSabado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            funciones funcion = new funciones();
+           // Usuarios usuarionew = new Usuarios();
+            int opc;
+            Console.WriteLine("Bienvenido al sistemas de Usuarios");
+            Console.WriteLine("1: Entrar como usuario");
+            Console.WriteLine("2: Entrar como invitado");
+            Console.WriteLine("3: Salir");
+            Console.WriteLine("Ingrese una opc");
+            do
+            {
+                opc = int.Parse(Console.ReadLine());
+                int opc2;
+                switch (opc)
+                {
+                    case 1:
+                        {
+                            bool banderaUsuario = false;
+                            bool banderaprivi = false;
+                            do
+                            { 
+                                Console.WriteLine("Bienvenido de vuelta Usuario");
+                                string nombre, contrasenia;
+                                Console.WriteLine("Ingrese nombre de Usuario");
+                                nombre = Console.ReadLine();
+                                Console.WriteLine("Ingrese contrasena");
+                                contrasenia = Console.ReadLine();
+                                banderaUsuario =funcion.Ingresar(nombre,contrasenia);
+                                banderaprivi=funcion.privilegios(nombre);
+                                if (banderaprivi == true)
+                                {
+                                    Console.WriteLine("------------------------------------------");
+                                    do
+                                    {
+                                        Console.WriteLine("Bienvenido Al Menu Administrador");
+                                        Console.WriteLine("1:Crear nuevo usuario");
+                                        Console.WriteLine("2:Cambiar contrasena");
+                                        Console.WriteLine("3:Borrar Usuario");
+                                        Console.WriteLine("4:Listado de Usuarios");
+                                        Console.WriteLine("5:Salir");
+                                        opc2 = int.Parse(Console.ReadLine());
+                                        switch (opc2)
+                                        {
+                                            case 1:
+                                                {
+                                                    Usuarios usuarionew = new Usuarios();
+                                                    funcion.crearUsuario(usuarionew);
+                                                    break;
+                                                }
+                                            case 2:
+                                                {
+                                                    funcion.CambiarContraseña(nombre,contrasenia);
+                                                    break;
+                                                }
+                                            case 3:
+                                                {
+
+                                                    break;
+                                                }
+                                            case 4:
+                                                {
+                                                    break;
+                                                }
+                                        }
+                                    } while (opc != 5);
+                                }
+                                else
+                                {
+                                    do
+                                    {
+                                        Console.WriteLine("Bienvenido Al Menu Usuario");
+                                        Console.WriteLine("1:Cambiar contrasena");
+                                        Console.WriteLine("2:Listado de Usuarios");
+                                        Console.WriteLine("3:Salir");
+                                        opc2 = int.Parse(Console.ReadLine());
+                                    } while (opc != 3);
+                                }
+                                
+
+                            } while (banderaUsuario!=false);
+                            break;
+                        }
+                    case 2:
+                        {
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Gracias por usar el Sistema");
+                            break;
+                        }
+                }
+
+            } while (opc != 3);
+        }
+    }
+}
---
+++ Back-end/Semana 16/Ejercicio/funciones.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSabado
+{
+    class funciones
+    {
+        Conexion con = new Conexion();
+        SqlCommand cmd;
+
+        public bool Ingresar(string nombre,string contrasenia)
+        {
+            bool bandera = false;
+            cmd = new SqlCommand($"SELECT count(*) FROM Usuario where nombre= '{nombre}' and contrasena = '{contrasenia}'",con.GetConexion());
+            con.AbrirConexion();
+            if (Convert.ToInt32(cmd.ExecuteScalar())!= 0)
+            {
+                Console.WriteLine($"Bienvenido Usuario '{nombre}'");
+                bandera = true;
+                con.CerrarConexion();
+                return bandera; 
+            }
+            else
+            {
+                Console.WriteLine("ERROR NO EXISTE Usuario");
+                con.CerrarConexion();
+                return bandera;
+               
+            }
+        }
+        public bool privilegios(string nombre)
+        {
+            cmd = new SqlCommand($"SELECT count(*) FROM Usuario where nombre= '{nombre}' and privilegios = '{1}'", con.GetConexion());
+            con.AbrirConexion();
+            if (Convert.ToInt32(cmd.ExecuteScalar()) != 0)
+            {
+                Console.WriteLine($"Posee privilegios de Administrador : '{nombre}'");
+                return true;
+                con.CerrarConexion();
+            }
+            else
+            {
+                Console.WriteLine("No Posee privilegios de Administrador");
+                return false;
+                con.CerrarConexion();
+            }
+        }
+        public void crearUsuario(Usuarios usuario)
+        {
+            cmd = new SqlCommand
+               ($"INSERT INTO USUARIOS (nombre, contrasena,correo,fecha,privilegios) " +
+               $"VALUES ('{usuario.Nombre}', '{usuario.Contrasenia}','{usuario.Correo}','{usuario.Fecha}','{usuario.Privilegios}')",
+               con.GetConexion()); 
+            con.AbrirConexion();  
+            cmd.ExecuteNonQuery();  
+            con.CerrarConexion();
+        }
+
+        public void CambiarContraseña(string nombre,string contrasenia)
+        {
+            cmd = new SqlCommand
+               ($"UPDATE USUARIOS SET contrasena = '{contrasenia}' " +
+               $"WHERE NOMBRE = '{nombre}'", con.GetConexion());
+            con.AbrirConexion();
+            cmd.ExecuteNonQuery();
+            con.CerrarConexion();
+        }
+        public void BorrarUsuario()
+        {
+            Console.WriteLine("Ingresar nombre de usuario");
+            string nombre = Console.ReadLine();
+
+            cmd = new SqlCommand
+               ($"DELETE FROM USUARIOS " +
+               $"WHERE NOMBRE = '{nombre}'", con.GetConexion());
+            cmd.ExecuteNonQuery();
+        }
+
+    }
+}
